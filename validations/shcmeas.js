0535886712df0aa2132974fad6d62330264865ad
Guney Ural <guneyural@gmail.com>
2021-01-27 18:24:36 +0300

    joi validation

---
+++ validations/shcmeas.js
@@ -0,0 +1,34 @@
+const Joi = require("joi");
+
+const UserValidation = Joi.object({
+  username: Joi.string().required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().required(),
+});
+
+const ShopValidation = Joi.object({
+  name: Joi.string().required(),
+  description: Joi.string(),
+  image: Joi.string(),
+});
+
+const CartValidation = Joi.object({
+  items: Joi.array(),
+  cartTotal: Joi.number(),
+});
+
+const ProductValidation = Joi.object({
+  title: Joi.string().required(),
+  price: Joi.string().required(),
+  description: Joi.string().required(),
+  stock: Joi.number().required(),
+  image: Joi.string().required(),
+  shop: Joi.string(),
+});
+
+module.exports = {
+  UserValidation,
+  ShopValidation,
+  CartValidation,
+  ProductValidation,
+};
