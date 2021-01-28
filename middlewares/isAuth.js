57af2c21dd9d84c04ac81cc31e255ffb24e8e329
Guney Ural <guneyural@gmail.com>
2021-01-28 10:12:07 +0300

    add middlewares

---
+++ middlewares/isAuth.js
@@ -0,0 +1,30 @@
+const jwt = require("jsonwebtoken");
+const expressError = require("../utils/expressError");
+
+module.exports.isUser = function (req, res, next) {
+  const token = req.headers["user-token"];
+
+  if (!token) next(new expressError("Login To See The Content.", 401));
+
+  try {
+    const decoded = jwt.verify(token, process.env.SECRET);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    next(new expressError("Login to See The Content", 401));
+  }
+};
+
+module.exports.isShop = function (req, res, next) {
+  const token = req.headers["shop-token"];
+
+  if (!token) next(new expressError("Login To See The Content.", 401));
+
+  try {
+    const decoded = jwt.verify(token, process.env.SECRET);
+    req.shop = decoded;
+    next();
+  } catch (err) {
+    next(new expressError("Login to See The Content", 401));
+  }
+};
