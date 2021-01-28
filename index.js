d487c65bbdd041ba8e13c1a86c57f47201c96ab5
Guney Ural <guneyural@gmail.com>
2021-01-28 10:11:30 +0300

    use helmet

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ const PORT = 3001 || process.env.PORT;
 const DATABASE = process.env.DATABASE;
 const expressError = require("./utils/expressError");
 const Routes = require("./routes");
+const helmet = require("helmet");
 
+app.use(helmet());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -21,20 +23,21 @@ mongoose
   .then(() => console.log("Connected To Database."))
   .catch((err) => console.log("Could Not Connect To Database." + err));
 
-app.use("/user", Routes.UserRoutes);
-app.use("/shop", Routes.ShopRoutes);
-app.use("/cart", Routes.CartRoutes);
-app.use("/product", Routes.ProductRoutes);
+app.use("/api/user", Routes.UserRoutes);
+app.use("/api/shop", Routes.ShopRoutes);
+app.use("/api/cart", Routes.CartRoutes);
+app.use("/api/product", Routes.ProductRoutes);
 app.get("/", (req, res) => {
   res.send("Güney Ural");
 });
 
 app.all("*", (req, res, next) => {
-  next(expressError("Could Not Find Route.", 404));
+  next(new expressError("Could Not Find Route.", 404));
 });
 
 app.use((err, req, res, next) => {
   const { statusCode = 500 } = err;
+  console.log(err);
   if (!err.message) err.message = "Something went wrong";
   res.status(statusCode).json({ statusCode, errorMessage: err.message });
 });
