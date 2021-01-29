9018617808c2bc037f35ddb75cceea2327de3e02
Guney Ural <guneyural@gmail.com>
2021-01-29 15:27:33 +0300

    create shop routes

--- routes/shopRoutes.js
+++ routes/shopRoutes.js
@@ -1,3 +1,15 @@
 const Router = require("express").Router();
+const {
+  createShop,
+  loginShop,
+  getShopById,
+  getCurrentShop,
+} = require("../controllers/shop");
+const { isShop } = require("../middlewares/isAuth");
+
+Router.post("/create", createShop);
+Router.post("/login", loginShop);
+Router.get("/p/:id", getShopById);
+Router.get("/current", isShop, getCurrentShop);
 
 module.exports = Router;
