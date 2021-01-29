34f4bbf5fdeda2272eb8b839184a9bebfde6e0f8
Guney Ural <guneyural@gmail.com>
2021-01-29 15:36:36 +0300

    update models

--- models/shop.js
+++ models/shop.js
@@ -1,14 +1,18 @@
 const mongoose = require("mongoose");
 const strOptions = { type: String, required: true };
+const bcrypt = require("bcrypt");
 
 const shopSchema = new mongoose.Schema(
   {
     name: strOptions,
+    email: strOptions,
     description: String,
     image: String,
   },
   { timestamps: true }
 );
+
+shopSchema.pre("save", async function (next) {});
 
 const Shop = mongoose.model("Shop", shopSchema);
 
--- models/user.js
+++ models/user.js
@@ -7,10 +7,12 @@ const userSchema = new mongoose.Schema(
     username: {
       type: String,
       max: 30,
+      unique: true,
       required: true,
     },
     email: {
       type: String,
+      unique: true,
       required: true,
     },
     password: {
