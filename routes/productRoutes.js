a0fa3f0ad2a7c65597651a5cca8731ed7e705944
Guney Ural <guneyural@gmail.com>
2021-01-30 02:20:56 +0300

    create product routes

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,3 +1,15 @@
 const Router = require("express").Router();
+const {
+  createProduct,
+  getProductById,
+  updateProduct,
+  deleteProduct,
+} = require("../controllers/product");
+const { isShop } = require("../middlewares/isAuth");
+
+Router.post("/new", isShop, createProduct);
+Router.put("/:id/update", isShop, updateProduct);
+Router.delete("/:id/delete", isShop, deleteProduct);
+Router.get("/:id", getProductById);
 
 module.exports = Router;
