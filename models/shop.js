63040618235aa73fde39ecfddee7d6a6276c0f3f
Guney Ural <guneyural@gmail.com>
2021-01-29 15:51:23 +0300

    add  middleware and update schema

--- models/shop.js
+++ models/shop.js
@@ -1,18 +1,28 @@
 const mongoose = require("mongoose");
 const strOptions = { type: String, required: true };
 const bcrypt = require("bcrypt");
+const expressError = require("../utils/expressError");
 
 const shopSchema = new mongoose.Schema(
   {
     name: strOptions,
     email: strOptions,
+    password: strOptions,
     description: String,
     image: String,
   },
   { timestamps: true }
 );
 
-shopSchema.pre("save", async function (next) {});
+shopSchema.pre("save", async function (next) {
+  const shop = this;
+  try {
+    const hashed = await bcrypt.hash(shop.password, 10);
+    this.password = hashed;
+  } catch (err) {
+    next(new expressError("An Error Occured While Creating Shop.", 500));
+  }
+});
 
 const Shop = mongoose.model("Shop", shopSchema);
 
