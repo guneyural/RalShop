463604a9ee1ab52f5cbb94635d9f433315196f13
Guney Ural <guneyural@gmail.com>
2021-01-30 14:06:08 +0300

    bug fix

--- models/shoppingCart.js
+++ models/shoppingCart.js
@@ -17,6 +17,6 @@ const cartSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-const Cart = mongoose.model("Cart", userSchema);
+const Cart = mongoose.model("Cart", cartSchema);
 
 module.exports = Cart;
