8d6e55144aa793759ad26df920eedd3f097376b3
Guney Ural <guneyural@gmail.com>
2021-01-30 03:00:54 +0300

    add product controller

---
+++ controllers/product.js
@@ -0,0 +1,62 @@
+const catchAsync = require("../utils/catchAsync");
+const expressError = require("../utils/expressError");
+const Product = require("../models/product");
+const { ProductValidation } = require("../validations/shcmeas");
+const mongoId = require("mongoose").Types.ObjectId;
+
+const createProduct = catchAsync(async (req, res, next) => {
+  const { title, price, description, stock, image } = req.body;
+  if (!title || !price || !description || !stock || !image)
+    return next(new expressError("Fill All Fields", 400));
+  const { error } = ProductValidation.validate(req.body);
+  if (error) return next(new expressError(error.details[0].message, 400));
+
+  const newProduct = new Product(req.body);
+  newProduct.shop = req.shop.id;
+  const product = await newProduct.save();
+  res.status(201).json(product);
+});
+
+const getProductById = catchAsync(async (req, res, next) => {
+  if (!mongoId.isValid(req.params.id))
+    return next(new expressError("Enter Valid Id.", 400));
+  const getProduct = await Product.findById(req.params.id);
+  if (!getProduct) return next(new expressError("Product Not Found", 404));
+  res.json(getProduct);
+});
+
+const updateProduct = catchAsync(async (req, res, next) => {
+  if (!mongoId.isValid(req.params.id))
+    return next(new expressError("Enter Valid Id.", 400));
+
+  const getProduct = await Product.findById(req.params.id);
+  if (!getProduct) return next(new expressError("Product Not Found", 404));
+  if (getProduct.shop === req.shop.id) {
+    const updatedProduct = await Product.findByIdAndUpdate(
+      req.params.id,
+      req.body
+    );
+    res.status(204).json(updatedProduct);
+  }
+  return next(new expressError("You Are Not Owner Of This Product.", 403));
+});
+
+const deleteProduct = catchAsync(async (req, res, next) => {
+  if (!mongoId.isValid(req.params.id))
+    return next(new expressError("Enter Valid Id.", 400));
+
+  const getProduct = await Product.findById(req.params.id);
+  if (!getProduct) return next(new expressError("Product Not Found", 404));
+  if (getProduct.shop === req.shop.id) {
+    await Product.deleteOne({ _id: req.params.id });
+    res.status(204).json("Product Deleted");
+  }
+  return next(new expressError("You Are Not Owner Of This Product.", 403));
+});
+
+module.exports = {
+  createProduct,
+  getProductById,
+  updateProduct,
+  deleteProduct,
+};
