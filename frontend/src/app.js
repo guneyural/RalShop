17e308893f5b2e18be6781edcf4aac85d6a9237d
Guney Ural <guneyural@gmail.com>
2021-01-31 16:58:09 +0300

    setup frontend

---
+++ frontend/src/app.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Güney Ural</h1>
+      <p>Güney Ural Web Developer</p>
+    </div>
+  );
+};
+
+export default App;
