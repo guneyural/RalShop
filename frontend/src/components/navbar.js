18d2ddd214be1573d2d19810d506300ebebcdc70
Guney Ural <guneyural@gmail.com>
2021-02-02 03:09:05 +0300

    navbar

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 import Logo from "../assets/logo.png";
@@ -9,6 +9,8 @@ import {
   FaHeart,
 } from "react-icons/fa";
 import { GiHamburgerMenu } from "react-icons/gi";
+import { categories, subCategories } from "../data/category";
+import { CgMenuGridR } from "react-icons/cg";
 
 const TinyNavLink = styled.span`
   color: #6c757d;
@@ -19,69 +21,187 @@ const TinyNavLink = styled.span`
 `;
 
 const Navbar = () => {
+  const [isNavOpen, setIsNavOpen] = useState(false);
+  const [windowSize, setWindowSize] = useState(window.innerWidth);
+
+  useEffect(() => {
+    document.querySelectorAll(".nav-mobile a").forEach((item) => {
+      item.addEventListener("click", () => setIsNavOpen(false));
+    });
+  }, []);
+
+  useEffect(() => {
+    if (isNavOpen) {
+      document.querySelector(".nav-mobile").className += " nav-active";
+      document.querySelector(".bg").style.display = "block";
+    } else {
+      document.querySelector(".nav-mobile").className = "nav-mobile";
+      document.querySelector(".bg").style.display = "none";
+    }
+  }, [isNavOpen]);
+
+  useEffect(() => {
+    const getWidth = () => {
+      setWindowSize(window.innerWidth);
+    };
+    window.addEventListener("resize", getWidth);
+
+    if (windowSize > 892) {
+      document.body.style.overflow = "auto";
+      setIsNavOpen(false);
+    } else if (windowSize <= 892 && isNavOpen) {
+      document.body.style.overflow = "hidden";
+    }
+
+    return () => {
+      window.removeEventListener("resize", getWidth);
+    };
+  }, [windowSize]);
+
+  const getCategory = (idx) => {
+    window.location.href = `http://localhost:3000/category/${idx}`;
+  };
+
   return (
-    <div className="navbar-section">
-      <div className="tiny-nav">
-        <div className="container">
-          <section className="tiny-nav-left">
-            <p className="text-muted">
-              Become a seller and grow your business.
-            </p>
-          </section>
-          <section className="tiny-nav-right">
-            <Link
-              to="/seller/register"
-              style={{ textDecoration: "none", marginRight: "10px" }}
-            >
-              <TinyNavLink>Be A Seller</TinyNavLink>
-            </Link>
-            <Link to="/seller/login" style={{ textDecoration: "none" }}>
-              <TinyNavLink>Login As Seller</TinyNavLink>
-            </Link>
-          </section>
+    <>
+      <div className="navbar-section">
+        <div className="tiny-nav">
+          <div className="container">
+            <section className="tiny-nav-left">
+              <p className="text-muted">
+                Become a seller and grow your business.
+              </p>
+            </section>
+            <section className="tiny-nav-right">
+              <Link
+                to="/seller/register"
+                style={{ textDecoration: "none", marginRight: "10px" }}
+              >
+                <TinyNavLink>Be A Seller</TinyNavLink>
+              </Link>
+              <Link to="/seller/login" style={{ textDecoration: "none" }}>
+                <TinyNavLink>Login As Seller</TinyNavLink>
+              </Link>
+            </section>
+          </div>
+        </div>
+        <div className="top-nav">
+          <div className="container">
+            <section className="navbar-left">
+              <Link
+                to="/"
+                style={{
+                  color: "var(--primary-color)",
+                  textDecoration: "none",
+                }}
+              >
+                <img src={Logo} alt="logo" className="nav-logo" />
+                <span className="logo-text">UralShop</span>
+              </Link>
+            </section>
+            <section className="navbar-middle">
+              <form onSubmit={(e) => e.preventDefault()}>
+                <input
+                  type="text"
+                  name="searchQuery"
+                  placeholder="Search Product, Brand, Category or Seller"
+                  className="search-bar"
+                />
+                <button className="navbar-search-icon">
+                  <FaSearch />
+                </button>
+              </form>
+            </section>
+            <section className="navbar-right">
+              <Link to="/auth">
+                <FaUserCircle /> Login
+              </Link>
+              <Link to="/wishlist">
+                <FaHeart /> Wishlist
+              </Link>
+              <Link to="/cart">
+                <FaShoppingCart /> Shopping Cart
+              </Link>
+              <button
+                className="burger-icon"
+                onClick={() => setIsNavOpen(true)}
+              >
+                <GiHamburgerMenu />
+              </button>
+            </section>
+          </div>
+          <div className="container">
+            <ul className="category-list">
+              {categories.map((category, i) => {
+                return (
+                  <li
+                    key={i}
+                    className="nav-category"
+                    onClick={() => getCategory(i)}
+                  >
+                    <span>{category}</span>
+                    <div className="dropdown-content">
+                      {subCategories[i] !== undefined &&
+                        subCategories[i].map((sub, idx) => {
+                          return (
+                            <li key={idx}>
+                              <Link to={`/category/${i}`}>{sub}</Link>
+                            </li>
+                          );
+                        })}
+                    </div>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
         </div>
       </div>
-      <div className="top-nav">
-        <div className="container">
-          <section className="navbar-left">
-            <Link
-              to="/"
-              style={{ color: "var(--primary-color)", textDecoration: "none" }}
-            >
-              <img src={Logo} alt="logo" className="nav-logo" />
-              <span className="logo-text">UralShop</span>
-            </Link>
-          </section>
-          <section className="navbar-middle">
-            <form onSubmit={(e) => e.preventDefault()}>
-              <input
-                type="text"
-                name="searchQuery"
-                placeholder="Search Product, Brand, Category or Seller"
-                className="search-bar"
-              />
-              <button className="navbar-search-icon">
-                <FaSearch />
-              </button>
-            </form>
-          </section>
-          <section className="navbar-right">
-            <Link to="/auth">
-              <FaUserCircle /> Login
-            </Link>
+      <div className="nav-mobile">
+        <section className="nav-mobile-top">
+          <button className="burger-icon" onClick={() => setIsNavOpen(false)}>
+            <GiHamburgerMenu />
+          </button>
+          <span className="mobile-logo-text">UralShop</span>
+        </section>
+        <section className="mobile-nav-middle">
+          <ul className="mobile-nav-links">
             <Link to="/wishlist">
-              <FaHeart /> Wishlist
+              <li>
+                <FaHeart className="mobile-nav-icon" /> Wishlist
+              </li>
             </Link>
             <Link to="/cart">
-              <FaShoppingCart /> Shopping Cart
+              <li>
+                <FaShoppingCart className="mobile-nav-icon" /> Shopping Cart
+              </li>
             </Link>
-            <button className="burger-icon">
-              <GiHamburgerMenu />
-            </button>
-          </section>
-        </div>
+            <Link to="/categories">
+              <li>
+                <CgMenuGridR className="mobile-nav-icon" /> Categories
+              </li>
+            </Link>
+            <Link to="/auth">
+              <li>
+                <FaUserCircle className="mobile-nav-icon" /> Login
+              </li>
+            </Link>
+          </ul>
+        </section>
+        <section className="mobile-nav-bottom">
+          <Link
+            to="/"
+            style={{
+              color: "var(--primary-color)",
+              textDecoration: "none",
+            }}
+          >
+            <img src={Logo} alt="logo" className="mobile-logo" />
+            <span className="mobile-logo-text">UralShop</span>
+          </Link>
+        </section>
       </div>
-    </div>
+    </>
   );
 };
 
