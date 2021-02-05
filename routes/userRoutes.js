c7785caa4383c02e44b26ca128e2e29ed298628f
Guney Ural <guneyural@gmail.com>
2021-02-05 20:23:51 +0300

    add new routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,5 +6,8 @@ Router.post("/register", userController.register);
 Router.post("/login", userController.login);
 Router.get("/p/:username", userController.getUserByUsername);
 Router.get("/current", isUser, userController.getCurrentUser);
+Router.get("/resetPassword", isUser, userController.resetPassword);
+Router.delete("/remove", isUser, userController.removeUser);
+Router.put("/", isUser, userController.updateUserData);
 
 module.exports = Router;
