7e5d1c377192654643288865a01b410d03d141d5
Guney Ural <guneyural@gmail.com>
2021-03-22 02:06:31 +0300

    add subscriber to redux store

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,6 +1,8 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import reducers from "./reducers";
+import axios from "axios";
+import { GET_WISHLIST, ADD_WISHLIST, REMOVE_WISHLIST } from "./actions/types";
 
 const initialState = {};
 const middleware = [thunk];
@@ -13,5 +15,34 @@ const Store = createStore(
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
+
+const tokenConfig = () => {
+  const token = localStorage.getItem("user-token");
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+  if (token) config.headers["user-token"] = token;
+  return config;
+};
+
+Store.subscribe(() => {
+  const {
+    Wishlist: { products },
+    lastAction,
+    Auth,
+  } = Store.getState();
+  if (Auth.isAuthenticated) {
+    localStorage.setItem("Wishlist", JSON.stringify(products));
+    if (
+      lastAction === ADD_WISHLIST ||
+      lastAction === REMOVE_WISHLIST ||
+      lastAction === GET_WISHLIST
+    ) {
+      axios.post("/api/wishlist/update", { products }, tokenConfig());
+    }
+  }
+});
 
 export default Store;
