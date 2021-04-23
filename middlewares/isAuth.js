33aaba7ea4bf91509bb13965cae72fe2547abb7f
Guney Ural <guneyural@gmail.com>
2021-04-23 16:38:50 +0300

    add authentication middleware for chat

--- middlewares/isAuth.js
+++ middlewares/isAuth.js
@@ -30,3 +30,24 @@ module.exports.isShop = function (req, res, next) {
     next(new expressError("Login to See The Content", 401));
   }
 };
+
+module.exports.isLoggedIn = function (req, res, next) {
+  const userToken = req.headers["user-token"];
+  const shopToken = req.headers["shop-token"];
+
+  if (!userToken && !shopToken) {
+    next(new expressError("Login To See The Content", 401));
+  }
+
+  try {
+    const decoded = jwt.verify(
+      userToken ? userToken : shopToken,
+      process.env.SECRET
+    );
+    req.user = userToken ? decoded : null;
+    req.shop = userToken ? null : decoded;
+    next();
+  } catch (err) {
+    next(new expressError("Login To See The Content", 401));
+  }
+};
