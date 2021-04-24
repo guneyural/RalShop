3d5fa672e01a16b55e4b099239a0fb780e3100b2
Guney Ural <guneyural@gmail.com>
2021-04-24 19:28:16 +0300

    chat page

--- frontend/src/pages/Messenger.js
+++ frontend/src/pages/Messenger.js
@@ -1,4 +1,7 @@
 import React, { useState, useEffect, useRef } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import LoadingIcon from "../assets/loading.gif";
+import { getChatrooms } from "../redux/actions/chatActions";
 import Styled from "styled-components";
 import io from "socket.io-client";
 
@@ -7,19 +10,41 @@ const ChatCount = Styled.p`
      color: var(--text-muted);
      margin-top: -10px;
 `;
+const LoadingGif = Styled.img`
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  height:100px;
+`;
+const InfoText = Styled.p`
+  margin:auto;
+  font-weight:500;
+  color: var(--text-muted);
+  text-align: center;
+`;
 
 const Messenger = () => {
+  const Chat = useSelector((state) => state.Chat);
+  const { inSellerRoute } = useSelector((state) => state.Seller);
+  const dispatch = useDispatch();
   const socketRef = useRef();
 
   useEffect(() => {
     socketRef.current = io.connect("/");
+    dispatch(getChatrooms(inSellerRoute ? true : false));
   }, []);
 
   return (
     <div>
       <h2 className="text-center">UralShop Chat</h2>
-      <ChatCount className="text-center">0 Chats</ChatCount>
+      <ChatCount className="text-center">
+        {Chat.chatrooms.length} Chats
+      </ChatCount>
       <hr />
+      <div className="main-area">
+        {Chat.loading && <LoadingGif src={LoadingIcon} alt="loading icon" />}
+        {Chat.chatrooms.length < 1 ? <InfoText>No Chat Found</InfoText> : ""}
+      </div>
     </div>
   );
 };
