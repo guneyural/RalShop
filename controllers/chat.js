485dc610a75c7891383b6468ff9691041e62234c
Guney Ural <guneyural@gmail.com>
2021-05-01 20:37:30 +0300

    add comments to chat code

--- controllers/chat.js
+++ controllers/chat.js
@@ -36,6 +36,7 @@ const createRoom = catchAsync(async (req, res, next) => {
 });
 
 const getChatrooms = catchAsync(async (req, res, next) => {
+  // Check if user exists
   if (req.user) {
     const getUser = await User.findById(req.user.id);
     if (!getUser) return next(new expressError("User Does Not Exist", 404));
@@ -44,23 +45,31 @@ const getChatrooms = catchAsync(async (req, res, next) => {
     const getShop = await Shop.findById(req.shop.id);
     if (!getShop) return next(new expressError("Shop Does Not Exist", 404));
   }
+  // set user variable equal to current user
   const user = req.user ? req.user : req.shop;
+  // get all the messages that the user sent or received
   const getRecentMessages = await Message.find({
     $or: [{ sender: user.id }, { receiver: user.id }],
   }).sort({ createdAt: "desc" });
-
+  // extract the chatroom ids from messages and append all ids to set
+  // because in sets you cant duplicate
   let rooms = new Set();
   getRecentMessages.forEach((message) => {
     rooms.add(message.chatroom);
   });
   let roomIds = [...rooms];
+
+  // get all the chatrooms in roomIds array
   const allChatrooms = await Chatroom.find({
     _id: { $in: [...roomIds] },
   })
     .sort({ createdAt: "desc" })
     .populate("participant creator");
+
+  // get all chatrooms and get the last message
   let chatroomArray = [];
   allChatrooms.forEach((chatroom) => {
+    // Get the newest message that's charoom id is the current chatroom's id
     chatroomArray.push({
       chatroom,
       lastMessage: getRecentMessages[0],
