661f3aff5cb348361fae6526005d9d500978d13a
Guney Ural <guneyural@gmail.com>
2021-05-17 20:38:30 +0300

    improve orders section

--- frontend/src/components/profilePageOrderSection.js
+++ frontend/src/components/profilePageOrderSection.js
@@ -1,10 +1,21 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import styled from "styled-components";
 import { priceConverter } from "../utils/helpers";
 import moment from "moment";
 import { Link, useHistory } from "react-router-dom";
+import { MdKeyboardArrowDown, MdKeyboardArrowUp } from "react-icons/md";
+import OrderDetailsModal from "./orderDetailsModal";
 
+const OrderBox = styled.div`
+  border: 1px solid #dbdbdb;
+`;
+const OrderBoxTop = styled.div`
+  border-bottom: 1.3px solid #dbdbdb;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
 const BoxHeaderText = styled.span`
   font-size: 14px;
   font-weight: bold;
@@ -15,29 +26,29 @@ const OrderHighlight = styled.span`
 const OrderItem = styled.div`
   border: 1px solid #dbdbdb;
   border-radius: 3px;
-  width: 80%;
+  width: 82%;
   margin: auto;
   margin-top: 15px;
   margin-bottom: 10px;
   padding: 3px 8px;
-  height: 250px;
+  height: 270px;
   position: relative;
   @media (max-width: 375px) {
-    width: 97%;
+    width: 98%;
   }
   @media (max-width: 1000px) {
-    height: 270px;
+    height: 290px;
   }
   @media (max-width: 767px) {
-    width: 93%;
-    height: 230px;
+    width: 94%;
+    height: 250px;
   }
   @media (max-width: 500px) {
-    height: 245px;
+    height: 265px;
   }
   @media (max-width: 375px) {
-    width: 95%;
-    height: 270px;
+    width: 96%;
+    height: 290px;
   }
 `;
 const OrderItemImage = styled.img`
@@ -111,22 +122,64 @@ const OrderBoxSettings = styled.div`
   margin-bottom: 0;
   display: flex;
   justify-content: flex-end;
+  align-items: center;
+  @media (max-width: 360px) {
+    padding: 0px;
+    padding-top: 8px;
+  }
 `;
 const OrderBoxSettingsItem = styled.p`
   padding: 0;
   margin: 0;
-  padding-right: 10px;
+  margin-right: 10px;
   font-size: 14px;
   color: var(--text-muted);
   cursor: pointer;
   &:hover {
     text-decoration: underline;
   }
+  @media (max-width: 360px) {
+    margin-right: 5px;
+  }
+  @media (max-width: 335px) {
+    font-size: 13px;
+  }
+`;
+const ShowOrderButton = styled.button`
+  border: none;
+  background: none;
+  font-weight: bold;
+  font-size: 15px;
+  margin: 0;
+  padding: 0;
+  padding-left: 8px;
+  &:focus {
+    outline: 0;
+  }
+  @media (max-width: 460px) {
+    font-size: 13px;
+    margin-right: -10px;
+  }
+`;
+const Row = styled.div`
+  padding: 3px 8px;
+  max-height: 0;
+  overflow: hidden;
+  -webkit-transition: max-height 0.8s;
+  -moz-transition: max-height 0.8s;
+  transition: max-height 0.8s;
+  transition-delay: 0;
+`;
+const WebsiteLink = styled.a`
+  text-decoration: none;
 `;
 
 const ProfilePageOrderSection = ({ setIsEmpty, isEmpty }) => {
   const dispatch = useDispatch();
   const { orders } = useSelector((state) => state.Order);
+  const [visibleOrder, setVisibleOrder] = useState("");
+  const [isOrderDetailsModalOpen, setIsOrderDetailsModalOpen] = useState(false);
+  const [orderDetailsGroupId, setOrderDetailsGroupId] = useState("");
   const history = useHistory();
 
   useEffect(() => {
@@ -137,6 +190,10 @@ const ProfilePageOrderSection = ({ setIsEmpty, isEmpty }) => {
   const productReviewsLink = (productId) => {
     history.push(`/product/${productId}/#reviews`);
   };
+  const openOrderDetailsModal = (groupId) => {
+    setOrderDetailsGroupId(groupId);
+    setIsOrderDetailsModalOpen(true);
+  };
 
   if (isEmpty) {
     return <h4>No Orders Added</h4>;
@@ -144,6 +201,12 @@ const ProfilePageOrderSection = ({ setIsEmpty, isEmpty }) => {
 
   return (
     <div>
+      {isOrderDetailsModalOpen && (
+        <OrderDetailsModal
+          setIsOrderDetailsModalOpen={setIsOrderDetailsModalOpen}
+          orderGroup={orders[orderDetailsGroupId]}
+        />
+      )}
       <div
         className="checkout-address-section-top w-100"
         style={{ position: "absolute", top: "0", left: "0" }}
@@ -153,61 +216,107 @@ const ProfilePageOrderSection = ({ setIsEmpty, isEmpty }) => {
       {Object.keys(orders).map((order, index) => {
         return (
           <div key={index}>
-            <div
+            <OrderBox
               className="checkout-address-section"
               style={index === 0 ? { marginTop: "35px" } : {}}
             >
-              <div className="checkout-address-section-top">
+              <OrderBoxTop className="checkout-address-section-top">
                 <OrderHighlight>
                   <span style={{ fontWeight: "bold" }}>
                     {orders[order].length}
                   </span>{" "}
                   products, on{" "}
                   <span style={{ fontWeight: "bold" }}>
-                    {moment(orders[order][0].createdAt).format("ll")}
+                    {moment(orders[order][0].order.createdAt).format("ll")}
                   </span>
                   , paid:{" "}
                   <span style={{ fontWeight: "bold" }}>
-                    {priceConverter(orders[order][0].totalAmount)}
+                    {priceConverter(orders[order][0].order.totalAmount)}
                   </span>
                 </OrderHighlight>
-              </div>
+                {visibleOrder !== order ? (
+                  <ShowOrderButton onClick={() => setVisibleOrder(order)}>
+                    Show <MdKeyboardArrowDown style={{ fontSize: "20px" }} />
+                  </ShowOrderButton>
+                ) : (
+                  <ShowOrderButton onClick={() => setVisibleOrder("")}>
+                    Hide <MdKeyboardArrowUp style={{ fontSize: "20px" }} />
+                  </ShowOrderButton>
+                )}
+              </OrderBoxTop>
               <OrderBoxSettings>
-                <OrderBoxSettingsItem>Send Receipt</OrderBoxSettingsItem>
+                <OrderBoxSettingsItem
+                  onClick={() => openOrderDetailsModal(order)}
+                >
+                  <b>Order Details</b>
+                </OrderBoxSettingsItem>
+                <WebsiteLink
+                  href={orders[order][0].Payment.receipt_url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <OrderBoxSettingsItem>View Receipt</OrderBoxSettingsItem>
+                </WebsiteLink>
                 <OrderBoxSettingsItem>Cancel Order</OrderBoxSettingsItem>
               </OrderBoxSettings>
-              <div className="row" style={{ padding: "3px 8px" }}>
+              <Row
+                className="row"
+                style={{
+                  maxHeight: visibleOrder === order ? "1000px" : "0",
+                }}
+              >
                 {orders[order].map((orderItem, idx) => {
                   return (
                     <div className="col-md-6" key={idx}>
                       <OrderItem>
-                        <Link to={`/product/${orderItem.Product.product._id}`}>
+                        <Link
+                          to={`/product/${orderItem.order.Product.product._id}`}
+                        >
                           <OrderItemImage
-                            src={orderItem.Product.product.images[0].url}
+                            src={orderItem.order.Product.product.images[0].url}
                             alt="product"
                           />
                         </Link>
                         <OrderItemTitle>
                           <span style={{ fontWeight: "bold", color: "#333" }}>
-                            {orderItem.seller.companyName}
+                            {orderItem.order.seller.companyName}
                           </span>{" "}
-                          {orderItem.Product.product.title.length > 55
-                            ? `${orderItem.Product.product.title.substring(
+                          {orderItem.order.Product.product.title.length > 55
+                            ? `${orderItem.order.Product.product.title.substring(
                                 0,
                                 55
                               )}...`
-                            : orderItem.Product.product.title}
+                            : orderItem.order.Product.product.title}
                         </OrderItemTitle>
                         <OrderItemPrice>
-                          {priceConverter(orderItem.Product.product.price)} -{" "}
+                          {priceConverter(
+                            orderItem.order.Product.product.price
+                          )}{" "}
+                          -{" "}
                           <OrderItemColorAndQty>
-                            qty: {orderItem.Product.quantity}
+                            qty: {orderItem.order.Product.quantity}
                           </OrderItemColorAndQty>
                           <ColorPreview
                             style={{
-                              background: "#" + orderItem.Product.color,
+                              background: "#" + orderItem.order.Product.color,
                             }}
                           />
+                        </OrderItemPrice>
+                        <OrderItemPrice
+                          style={{
+                            fontSize: "14px",
+                            color: "var(--text-muted)",
+                          }}
+                        >
+                          <span
+                            style={{
+                              fontWeight: "normal",
+                              textDecoration: "underline",
+                            }}
+                          >
+                            {orderItem.order.status === "waitingConfirmation" &&
+                              "Waiting confirmation by seller"}
+                          </span>
                         </OrderItemPrice>
                         <OrderItemBottom>
                           <OrderItemOptions
@@ -222,7 +331,9 @@ const ProfilePageOrderSection = ({ setIsEmpty, isEmpty }) => {
                           </OrderItemOptions>
                           <OrderItemOptions
                             onClick={() =>
-                              productReviewsLink(orderItem.Product.product._id)
+                              productReviewsLink(
+                                orderItem.order.Product.product._id
+                              )
                             }
                           >
                             Rate Product
@@ -232,8 +343,8 @@ const ProfilePageOrderSection = ({ setIsEmpty, isEmpty }) => {
                     </div>
                   );
                 })}
-              </div>
-            </div>
+              </Row>
+            </OrderBox>
           </div>
         );
       })}
