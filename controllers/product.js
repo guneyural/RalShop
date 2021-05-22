3d2f6e38fd3a81de3125da46af4c3eab87172f8e
Guney Ural <guneyural@gmail.com>
2021-05-22 21:44:00 +0300

    let seller to delete product

--- controllers/product.js
+++ controllers/product.js
@@ -1,9 +1,14 @@
 const Product = require("../models/product");
+const Order = require("../models/orders");
+const Review = require("../models/review");
 const Wishlist = require("../models/wishlist");
+const Cart = require("../models/shoppingCart");
+const Transporter = require("../nodemailer");
 const catchAsync = require("../utils/catchAsync");
 const expressError = require("../utils/expressError");
 const mongoId = require("mongoose").Types.ObjectId;
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const moment = require("moment");
 
 const createProduct = catchAsync(async (req, res, next) => {
   const {
@@ -96,8 +101,112 @@ const deleteProduct = catchAsync(async (req, res, next) => {
 
   const getProduct = await Product.findById(req.params.id);
   if (!getProduct) return next(new expressError("Product Not Found", 404));
+
   if (getProduct.shop == req.shop.id) {
+    const getOrder = await Order.find({
+      $and: [{ seller: req.shop.id }, { "Product.product": req.params.id }],
+    });
+
+    if (getOrder.length < 1) {
+      await Cart.updateMany({ $pull: { items: { product: req.params.id } } });
+      await Product.deleteOne({ _id: req.params.id });
+      await Review.deleteMany({ productId: req.params.id });
+      await Wishlist.updateMany({ $pull: { products: req.params.id } });
+    } else {
+      let firstEmail = {
+        from: process.env.EMAIL,
+        subject:
+          "One Of The Product That You Ordered Has Deleted By The Seller",
+        html: `<div style="margin:auto;background:white;border:1px solid #dedede;width:400px;padding:20px">
+          <h1>One of the product that you ordered has deleted by the seller.</h1>
+          <p>${
+            getProduct.title
+          } that you have ordered has been deleted by the seller. You paid ${priceConverter(
+          getProduct.price
+        )} for this product. You will be refunded.</p>
+       `,
+      };
+      let secondEmail = { ...firstEmail };
+
+      for (let item of getOrder) {
+        const getSpecificOrder = await Order.findOne({ groupId: item.groupId });
+        getSpecificOrder.note =
+          getProduct.title +
+          " Has been deleted by the seller. You've been refunded. You will only receive the product(s) remaining.";
+        await getSpecificOrder.save();
+        await item.remove();
+        await stripe.refunds.create({
+          payment_intent: item.paymentIntentId,
+          amount: getProduct.price * 100,
+        });
+
+        if (item.billingAddress.email === item.deliveryAddress.email) {
+          Transporter.sendMail(
+            {
+              ...firstEmail,
+              to: item.billingAddress.email,
+              html:
+                firstEmail.html +
+                `  You paid ${priceConverter(
+                  item.totalAmount
+                )} for the whole order, on ${moment(item.createdAt).format(
+                  "ll"
+                )}.
+                <p>You will get refunded ${priceConverter(getProduct.price)}</p>
+                <hr />
+                <h3 style="text-align:center">UralShop</h3>
+                <p style="text-align:center; font-size:11px;">Guney Ural @2021</p>
+              </div>`,
+            },
+            () => Transporter.close()
+          );
+        } else {
+          Transporter.sendMail(
+            {
+              ...firstEmail,
+              to: item.billingAddress.email,
+              html:
+                firstEmail.html +
+                `  You paid ${priceConverter(
+                  item.totalAmount
+                )} for the whole order, on ${moment(item.createdAt).format(
+                  "ll"
+                )}.
+                <p>You will get refunded ${priceConverter(getProduct.price)}</p>
+              <hr />
+              <h3 style="text-align:center">UralShop</h3>
+              <p style="text-align:center; font-size:11px;">Guney Ural @2021</p>
+            </div>`,
+            },
+            () => Transporter.close()
+          );
+          Transporter.sendMail(
+            {
+              ...secondEmail,
+              to: item.deliveryAddress.email,
+              html:
+                secondEmail.html +
+                `  You paid ${priceConverter(
+                  item.totalAmount
+                )} for the whole order, on ${moment(item.createdAt).format(
+                  "ll"
+                )}.
+                <p>You will get refunded ${priceConverter(getProduct.price)}</p>
+              <hr />
+              <h3 style="text-align:center">UralShop</h3>
+              <p style="text-align:center; font-size:11px;">Guney Ural @2021</p>
+            </div>`,
+            },
+            () => Transporter.close()
+          );
+        }
+      }
+    }
+
+    await Cart.updateMany({ $pull: { items: { product: req.params.id } } });
     await Product.deleteOne({ _id: req.params.id });
+    await Review.deleteMany({ productId: req.params.id });
+    await Wishlist.updateMany({ $pull: { products: req.params.id } });
     return res.json("Product Deleted");
   }
   return next(new expressError("You Are Not Owner Of This Product.", 403));
@@ -111,6 +220,16 @@ const getSellerAllProducts = catchAsync(async (req, res) => {
   });
   res.status(200).json(getAllProducts);
 });
+
+const priceConverter = (number) => {
+  const formatter = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+  });
+
+  return formatter.format(number);
+};
 
 module.exports = {
   createProduct,
