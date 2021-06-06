a1e461210f680b6c61d60f9d73fea83aaf70e090
Guney Ural <guneyural@gmail.com>
2021-06-06 19:42:40 +0300

    first commit

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -22,10 +22,7 @@ const middleware = [thunk];
 const Store = createStore(
   reducers,
   initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  compose(applyMiddleware(...middleware))
 );
 
 const tokenConfig = () => {
